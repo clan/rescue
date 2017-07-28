a2aa62ab3894ede3aa8c2fb2daf51055861f22e9
Z. Liu <zhixu.liu@gmail.com>
2017-07-29 06:25:27 +0800

    define config for files

--- gen_initramfs_img.sh
+++ gen_initramfs_img.sh
@@ -10,7 +10,7 @@ NORMAL="\033[0m"
 
 good_msg()
 {
-    msg=$1
+    msg="$@"
     msg="${msg:-...}"
 
     echo -e "${GOOD}>>${NORMAL} ${msg} ${NORMAL}"
@@ -18,10 +18,19 @@ good_msg()
 
 bad_msg()
 {
-    msg=$1
+    msg="$@"
     msg="${msg:-...}"
 
     echo -e "${BAD}>>${NORMAL} ${msg} ${NORMAL}"
+}
+
+die()
+{
+    local ret="${1:-0}"
+    shift
+
+    bad_msg "$@"
+    exit $ret
 }
 
 iter_dirname()
@@ -37,25 +46,60 @@ iter_dirname()
     done
 }
 
+gen_file()
+{
+    f=$1
+    if [[ "$#" -lt 2 ]] ; then
+        fn=${f}
+    else
+        fn=$2
+    fi
+
+    local list_files=""
+    if [ -h "${f}" ] ; then
+        ft=$(readlink -e "${f}")
+        list_files=${list_filess}"slink ${fn} ${ft} 0777 0 0\n"
+        fm=$(stat -c "%04a" "${ft}")
+        list_files=${list_files}"file ${ft} ${ft} ${fm} 0 0\n"
+    else
+        fm=$(stat -c "%04a" "${f}")
+        list_files=${list_files}"file ${fn} ${f} ${fm} 0 0\n"
+    fi
+
+    echo -e ${list_files}
+}
+
+ARGVS=$(getopt -o k: -- "$@") || die $? getopt failed.
+eval set -- "$ARGVS"
+
+KIND="initramfs"
+
+while true; do
+    case "$1" in
+        -k) KIND="$2"; shift 2;;
+        --) shift; break;;
+        *) die 1 "getopt internal error!";;
+    esac
+done
+
+PROG_LIST=${KIND}.conf
+if [ ! -s "${PROG_LIST}" ] ; then
+    die 1 "${PROG_LIST} not found"
+fi
+
 KV=$1
 [ -z "${KV}" ] && KV=$(uname -r)
 km_directory="/lib/modules/${KV}/"
 if [ ! -d "${km_directory}" ] ; then
-    bad_msg "modules directory for kernel ${KV} not found"
-    exit 1
+    die 1 "modules directory for kernel ${KV} not found"
 fi
-good_msg "build initramfs image for kernel ${KV}"
+good_msg "build initramfs image w/ kernel ${KV}"
 
-list_items="file /init ./init 0755 0 0
-slink /linuxrc init 0755 0 0
+list_items="
+slink /linuxrc init 0777 0 0
 
 # /bin
 dir /bin 0755 0 0
-file /bin/busybox /bin/busybox 0755 0 0
-slink /bin/echo busybox 0755 0 0
-slink /bin/sh busybox 0755 0 0
-file /bin/kmod /bin/kmod 0755 0 0
-file /bin/udevadm /bin/udevadm 0755 0 0
 
 dir /dev 0755 0 0
 nod /dev/console 0660 0 0 c 5 1
@@ -69,17 +113,82 @@ nod /dev/zero 0660 0 0 c 1 5
 dir /etc 0755 0 0
 
 dir /lib64 0755 0 0
-slink /lib lib64 0755 0 0
+slink /lib lib64 0777 0 0
 
 dir /run 0755 0 0
 
 dir /sbin 0755 0 0
-slink /sbin/depmod /bin/kmod 0755 0 0
 
 dir /usr 0755 0 0
 dir /usr/bin 0755 0 0
 dir /usr/sbin 0755 0 0
+dir /usr/lib64 0755 0 0
+slink /usr/lib lib64 0777 0 0
 "
+
+list_confs=""
+list_dirs="./${KIND}"
+source ${PROG_LIST}
+for g in ${!conf_*}; do
+    k=${g#conf_}
+    if [ "${k}" == "dirs" ] ; then
+        echo ">>> collect dirs"
+        for e in ${!g}; do
+            list_dirs=${list_dirs}" ${e}"
+        done
+    elif [ "${k}" == "files" ] ; then
+        for f in ${!g}; do
+	    list_confs=${list_confs}"$(gen_file ${f})\n"
+        done
+    elif [ "${k}" == "executables" ] ; then
+        echo ">>> collect executables"
+        for e in ${!g}; do
+            fe=$(which $e)
+            if [ "${e}" == "busybox" ] ; then
+                busybox_path=${fe}
+            fi
+	    list_confs=${list_confs}"$(gen_file ${fe})\n"
+        done
+    elif [ "${k}" == "link_to_busybox" ] ; then
+        echo ">>> collect link to busybox"
+        if [ -z "${busybox_path}" ] ; then
+            die 1 "busybox not included in executables, please fix!"
+        fi
+        for s in ${!g}; do
+            fe=$(which $s)
+            if [ "${fe%/*}" == "${busybox_path%/*}" ] ; then
+                fb=busybox
+            else
+                fb=${busybox_path}
+            fi
+            list_confs=${list_confs}"slink ${fe} ${fb} 0777 0 0\n"
+        done
+    fi
+done
+list_items=${list_items}"${list_confs}"
+
+list_confs=""
+for d in ${list_dirs}; do
+    if [ -d "${d}" ] ; then
+        for f in $(find ${d}); do
+            if [[ "${d}" =~ "./" ]] ; then
+                [[ "${f}" == "${d}" ]] && continue
+                fn=${f#${d}/}
+            else
+                fn=${f}
+            fi
+            if [[ "${fn:0:1}" != "/" ]] ; then
+                fn=/${fn}
+            fi
+            if [ -d ${f} ] ; then
+                list_confs=${list_confs}"dir ${fn} 0755 0 0\n"
+            else
+                list_confs=${list_confs}"$(gen_file ${f} ${fn})\n"
+            fi
+        done
+    fi
+done
+list_items=${list_items}"${list_confs}"
 
 good_msg "Collect udev files ..."
 
@@ -120,7 +229,7 @@ AWK_LDD='{
     }
 }'
 
-good_msg "Collect executables ..."
+good_msg "Find executables from lists ..."
 programs=$(echo -e "${list_items}" | awk "${AWK_EXEC}")
 
 good_msg "Collect libraries from executables ..."
@@ -176,7 +285,10 @@ for m in $(echo -e "${file_modules}" | sort | uniq); do
 done
 list_items=${list_items}${list_modules}
 
-good_msg "Build initramfs-${KV}.img"
+good_msg "Build ${KIND}-${KV}.img"
 echo -e "${list_items}" | \
+    sort -k2 | \
+    uniq | \
+    tee ${KIND}-${KV}.list | \
     /usr/src/linux/usr/gen_init_cpio - | \
-    xz -e --check=none -z -f -9 > initramfs-${KV}.img
+    xz -e --check=none -z -f -9 > ${KIND}-${KV}.img
